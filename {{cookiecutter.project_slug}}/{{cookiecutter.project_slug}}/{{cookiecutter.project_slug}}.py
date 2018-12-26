81377c2b593f82c6ec9f03ee5922c00a301cea8e
Javier Sánchez Portero <javiersanp@gmail.com>
2018-12-26 02:09:08 +0000

    Create code template

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -0,0 +1,11 @@
+# -*- coding=utf-8 -*-
+"""{{cookiecutter.project_name}} core module"""
+
+
+def get_sneks():
+    """Return a dict of sneks"""
+    return dict(
+        red="\033[91m🐍\033[0m",
+        green="\033[92m🐍\033[0m",
+        blue="\033[94m🐍\033[0m",
+    )
