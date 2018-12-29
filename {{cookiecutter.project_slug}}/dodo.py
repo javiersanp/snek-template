729df5aba71332cd12f244344504384ee8d3337c
Javier Sánchez Portero <javiersanp@gmail.com>
2018-12-29 15:50:03 +0000

    Improve doit check

--- {{cookiecutter.project_slug}}/dodo.py
+++ {{cookiecutter.project_slug}}/dodo.py
@@ -9,13 +9,12 @@ DOIT_CONFIG = {"default_tasks": ["task_check", "style"], "verbosity": 2}
 PYTHON_FILES = glob.glob("**/*.py", recursive=True)
 
 
-def get_subtask(action):
-    """Return a dictionary defining a substack for 'action'."""
-    return {
-        "file_dep": PYTHON_FILES,
-        "name": action.split(" ")[0],
-        "actions": [action],
-    }
+def get_subtask(cmd_action, file_dep=None):
+    """Return a dictionary defining a substack for string 'cmd_action'."""
+    task = {"name": cmd_action.split(" ")[0], "actions": [cmd_action]}
+    if file_dep is not None:
+        task["file_dep"] = file_dep
+    return task
 
 
 def temp_task_install():
@@ -26,7 +25,7 @@ def temp_task_install():
 def task_check():
     """Check diff of code formatters."""
     for action in ["black -l 79 --diff .", "isort --diff"]:
-        yield {"name": action.split(" ")[0], "actions": [action]}
+        yield get_subtask(action, PYTHON_FILES)
 
 
 def task_format():
